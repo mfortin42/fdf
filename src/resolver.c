6dbed226cef99ad399c47daaef7d7a6e47f027ae
Maxime FORTIN <m.fortin75017@gmail.com>
2016-02-20 19:38:38 +0100

    Update resolver.c

--- src/resolver.c
+++ src/resolver.c
@@ -93,31 +93,22 @@ void	ft_color(int x, int y, t_env *e, int direct)
 		calc = (e->actual_z > e->y_next) ? x : e->y_next;
 	if (calc == 0)
 		ft_put_pixel(e, x, y, 0xFFFFFF);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0xFFFFFF);
 	else if (calc < 2)
 		ft_put_pixel(e, x, y, 0x57CAFF);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x57CAFF);
 	else if (calc < 4)
 		ft_put_pixel(e, x, y, 0x4EB5E5);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x4EB5E5);
 	else if (calc < 8)
 		ft_put_pixel(e, x, y, 0x45A1CC);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x45A1CC);
 	else if (calc < 16)
 		ft_put_pixel(e, x, y, 0x3C8DB2);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x3C8DB2);
 	else if (calc < 32)
 		ft_put_pixel(e, x, y, 0x427999);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x427999);
 	else if (calc < 64)
 		ft_put_pixel(e, x, y, 0x2B657F);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x2B657F);
 	else if (calc < 128)
 		ft_put_pixel(e, x, y, 0x225066);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x225066);
 	else
 		ft_put_pixel(e, x, y, 0x1A3C4C);
-//		mlx_pixel_put(e->mlx, e->win, x, y, 0x1A3C4C);
 }
 
 void	ft_put_pixel(t_env *e, int x, int y, int color)
@@ -129,10 +120,3 @@ void	ft_put_pixel(t_env *e, int x, int y, int color)
 	tmp = (int *)&e->imc[(y * e->imlen) + (x * (e->bpp / 8))];
 	*tmp = color;
 }
-
-
-
-
-
-
-
